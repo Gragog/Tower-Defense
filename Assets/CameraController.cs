128aab60b614305077f3968f414dcb4f489ec45d
Gragog <36130475+Gragog@users.noreply.github.com>
2018-05-21 18:47:22 +0200

    Camera scrolling an limiting

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,35 +4,55 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    [Header("Movement")]
     [Range(20, 100f)]
-    public float panSpeed = 50f;
+    public float panSpeed = 35f;
     float targetPanSpeed;
     [Range(1f, 20f)]
     public float panAccelaration = 10f;
+    [Range(0f, 50f)]
+    public float scrollSpeed = 30f;
     [Range(1, 10)]
-    public byte boostMultiplier = 2;
+    public byte boostMultiplier = 3;
     [Range(0, 100)]
-    public int borderForMouse = 25;
+    public byte borderForMouse = 25;
 
     float currentPanSpeed;
     Vector3 targetDirection;
     Vector3 lastMovement;
 
+    Vector3 camStartPosition;
+
+    bool doMovement = true;
+
+    void Start()
+    {
+        camStartPosition = transform.position;
+    }
+
     void LateUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.Escape)) doMovement = !doMovement;
+        if (!doMovement) return;
+
         targetDirection = Vector3.zero;
         targetPanSpeed = panSpeed;
 
-        if (Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - borderForMouse)
+        if (transform.position.z < maxZ && (Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - borderForMouse))
             targetDirection.z = 1;
-        if (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= borderForMouse)
+        if (transform.position.z > minZ && (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= borderForMouse))
             targetDirection.z = -1;
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= borderForMouse)
+        if (transform.position.x > minX && (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= borderForMouse))
             targetDirection.x = -1;
-        if (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - borderForMouse)
+        if (transform.position.x < maxX && (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - borderForMouse))
             targetDirection.x = 1;
         if (Input.GetKey(KeyCode.RightShift))
             targetPanSpeed = panSpeed * boostMultiplier;
+        if (Input.GetMouseButtonDown(2))
+            transform.position = camStartPosition;
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        HandleScroll(-scroll * scrollSpeed);
 
         if (targetDirection.z != 0 || targetDirection.x != 0)
         {
@@ -52,5 +72,27 @@ public class CameraController : MonoBehaviour {
 
         if (currentPanSpeed != 0f)
             currentPanSpeed = 0f;
+    }
+
+    float currentScroll = 0f;
+    [Header("Restrictions")]
+    public float minY = 20f;
+    public float maxY = 100f;
+    public float minX = -25f;
+    public float maxX = 100f;
+    public float minZ = -35f;
+    public float maxZ = 50f;
+
+    void HandleScroll(float scrollAmount)
+    {
+        currentScroll = Mathf.Lerp(currentScroll, scrollAmount, Time.deltaTime * scrollSpeed * .2f);
+
+        if (transform.position.y > maxY && currentScroll > 0f)
+            return;
+        if (transform.position.y < minY && currentScroll < 0f)
+            return;
+
+        if (currentScroll > .1f || currentScroll < -.1f)
+            transform.Translate(Vector3.up * currentScroll, Space.World);
     }
 }
